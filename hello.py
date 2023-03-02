cc09849d6a47f77cf200a14dda7663913d5dada1
JYDE <124386662+Symplyjide@users.noreply.github.com>
2023-03-02 13:54:45 +0100

    Python Tutorial

---
+++ hello.py
@@ -0,0 +1,66 @@
+# name = input("Name:")
+# print(f"You've just begun {name}")
+#===================================
+
+# Conditions========== 
+# n = int(input("Number: "))
+
+# if n > 0 :
+#     print("n is positive")
+
+# elif n < 0:
+#     print("n is negative")
+
+# else: print("n is zero")
+#=================================
+
+#List,tuples,dictionary
+# names = ["harry", "ron", "hermione"]
+# names.append("draco")
+# names.sort()
+# print(names)
+#===================================================
+
+#SETS
+# create an empty set
+# s = set()
+
+# # Add elements to the set
+# s.add(1)
+# s.add(2)
+# s.add(3)
+# s.add(4)
+# s.add(5)
+# s.add(3)
+# s.remove(5)
+# print(f"the number of elements which is {len(s)} is correct")
+#=====================================================================
+
+# DICTIONARY
+
+# houses = {"Harry":"Gryffindor", "Draco": "Slytherin"}
+# houses["Hermione"] = "Gryffindor"
+
+# print(houses)
+#===============================================================
+
+# LOOPS
+
+# for i in [0, 1, 2, 3, 4, 5]:
+#     print(i)
+
+# for i in range(100):
+#       print(i)
+
+# names = ["Harry", "Hermione", "Ron"]
+# for name in names:
+#     print(name)
+
+# shops = ["Justrite", "Shoprite", "Hutoos"]
+# for shop in shops:
+#     print(shops)
+game = {"God of War":"Playstation", "Halo": "Xbox"}
+game["Spiderman"] = "Playstation"
+for i in game:
+    print(i)
+
